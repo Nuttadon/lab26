1afafc4e6b2c22f0a0cf5d7149fac1110a838b54
Nuttadon <nuttadon_p@cmu.ac.th>
2022-02-17 22:51:55 +0700

    lab26_3 complete

--- lab26_3.cpp
+++ lab26_3.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
-
+#include<set>
 using namespace std;
 
-//Write function count() here.
+int count(int d[],int size){
+	set<int> inter;
+	for(int i=0;i<size;i++){
+		inter.insert(d[i]);
+	}
+	return inter.size();
+}
 
 int main(){	
 	int data[] = {1,2,4,5,4,8,2,1,2,4,6,1,4,4,4,2,1,0,12}; 
